a2f7c437a3acee0b844cd6e1a6ff1e3ea9ed1d96
Octavian Iacob <octavian.iacob@icloud.com>
2019-12-12 12:33:15 +0200

    fixed showGenres

--- client.c
+++ client.c
@@ -1,8 +1,3 @@
-/* cliTCPIt.c - Exemplu de client TCP
-   Trimite un nume la server; primeste de la server "Hello nume".
-         
-   Autor: Lenuta Alboaie  <adria@infoiasi.ro> (c)2009
-*/
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -13,10 +8,7 @@
 #include <netdb.h>
 #include <string.h>
 
-/* codul de eroare returnat de anumite apeluri */
 extern int errno;
-
-/* portul de conectare la server*/
 int port;
 int loginFlag = 0;
 int adminFlag = 0;
@@ -34,36 +26,26 @@ void menu()
 
 int main(int argc, char *argv[])
 {
-  int sd;                    // descriptorul de socket
-  struct sockaddr_in server; // structura folosita pentru conectare
-  char msg[1000];            // mesajul trimis
+  int sd;
+  struct sockaddr_in server;
+  char msg[1000];
 
-  /* exista toate argumentele in linia de comanda? */
   if (argc != 3)
   {
     printf("Sintax: %s <server_address> <port>\n", argv[0]);
     return -1;
   }
 
-  /* stabilim portul */
   port = atoi(argv[2]);
-
-  /* cream socketul */
   if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
   {
     perror("Eroare la socket().\n");
     return errno;
   }
-
-  /* umplem structura folosita pentru realizarea conexiunii cu serverul */
-  /* familia socket-ului */
   server.sin_family = AF_INET;
-  /* adresa IP a serverului */
   server.sin_addr.s_addr = inet_addr(argv[1]);
-  /* portul de conectare */
   server.sin_port = htons(port);
 
-  /* ne conectam la server */
   if (connect(sd, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1)
   {
     perror("[client]Eroare la connect().\n");
--- server.c
+++ server.c
@@ -1,8 +1,3 @@
-/* servTcpConc.c - Exemplu de server TCP concurent
-    Asteapta un nume de la clienti; intoarce clientului sirul
-    "Hello nume".
-    */
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -13,16 +8,13 @@
 #include <stdlib.h>
 #include <sqlite3.h>
 
-/* portul folosit */
 #define PORT 2024
 #define BUF 1000
 
-/* codul de eroare returnat de anumite apeluri */
 extern int errno;
 
 char username[100], password[100];
 int loginFlag, adminFlag;
-// conexiune baza de date
 sqlite3 *database;
 sqlite3_stmt *statement;
 char *query = NULL;
@@ -156,64 +148,46 @@ int isLoggedIn(char username[], char password[])
 
 void insertSong(char song_name[], char song_artist[], char song_link[], char genre1, char genre2, char genre3)
 {
-	printf("A INTRAT PE FUNCTIE\n");
 	rc = sqlite3_open("topmusic.db", &database);
 	if (rc)
 		printf("Error opening database.\n");
 	else
 		printf("Database opened successfully.\n");
-	printf("INAINTE DE ASPRINTF\n");
 	//asprintf(&query, "insert into songs (song_name, song_artist, link, genre1, genre2, genre3) values (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\");", song_name, song_artist, song_link, genre1, genre2, genre3);
 	asprintf(&query, "insert into songs (song_name, song_artist, link, genre1) values (\"%s\", \"%s\", \"%s\", \"%s\");", song_name, song_artist, song_link);
 	printf("The following SQL Query will run: '%s'\n", query);
-	printf("INAINTE DE PREPARE V2\n");
-	printf("QUERY-UL ESTE: %s\n", query);
 	sqlite3_prepare_v2(database, query, strlen(query), &statement, NULL);
-	printf("DUPA PREPARE V2\n");
 	rc = sqlite3_step(statement);
-	printf("DUPA STEP\n");
 	if (rc != SQLITE_DONE)
 		printf("ERROR inserting data: %s\n", sqlite3_errmsg(database));
 	else
 		printf("Song inserted successfully.\n");
-	printf("DUPA SQLITE_DONE\n");
 	sqlite3_finalize(statement);
-	printf("DUPA FINALIZARE\n");
 	free(query);
 	sqlite3_close(database);
-	printf("DUPA INCHIDERE\n");
 }
 
 void insertGenre(char genre_name[], char genre_description[])
 {
-	printf("A INTRAT PE FUNCTIE\n");
 	rc = sqlite3_open("topmusic.db", &database);
 	if (rc)
 		printf("Error opening database.\n");
 	else
 		printf("Database opened successfully.\n");
-	printf("INAINTE DE ASPRINTF\n");
 	asprintf(&query, "insert into genres (genre_name, genre_description) values (\"%s\", \"%s\");", genre_name, genre_description);
 	printf("The following SQL Query will run: '%s'\n", query);
-	printf("INAINTE DE PREPARE V2\n");
-	printf("QUERY-UL ESTE: %s\n", query);
 	sqlite3_prepare_v2(database, query, strlen(query), &statement, NULL);
-	printf("DUPA PREPARE V2\n");
 	rc = sqlite3_step(statement);
-	printf("DUPA STEP\n");
 	if (rc != SQLITE_DONE)
 		printf("ERROR inserting data: %s\n", sqlite3_errmsg(database));
 	else
 		printf("Genre inserted successfully.\n");
-	printf("DUPA SQLITE_DONE\n");
 	sqlite3_finalize(statement);
-	printf("DUPA FINALIZARE\n");
 	free(query);
 	sqlite3_close(database);
-	printf("DUPA INCHIDERE\n");
 }
 
-char showGenres()
+void showGenres(char* out)
 {
 	int genre_count = 0;
 	char genresDisplay[1000];
@@ -246,53 +220,44 @@ char showGenres()
 	free(query);
 	sqlite3_close(database);
 	//printf("Variabila genresDisplay are:\n %s\n", genresDisplay);
-	return genresDisplay;
+	strcpy(out,genresDisplay);
 }
 
 int main()
 {
-	struct sockaddr_in server; // structura folosita de server
+	struct sockaddr_in server;
 	struct sockaddr_in from;
 	char input[1000];		 //mesajul primit de la client
 	char output[1000] = " "; //mesaj de raspuns pentru client
-	int sd;					 //descriptorul de socket
+	int sd;					 
 	char user_input[1000];   // copie a mesajului primit de la client
 
-	/* crearea unui socket */
 	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 	{
-		perror("Eroare la socket().\n");
+		perror("Error on socket().\n");
 		return errno;
 	}
 
-	/* pregatirea structurilor de date */
 	bzero(&server, sizeof(server));
 	bzero(&from, sizeof(from));
 
-	/* umplem structura folosita de server */
-	/* stabilirea familiei de socket-uri */
 	server.sin_family = AF_INET;
-	/* acceptam orice adresa */
 	server.sin_addr.s_addr = htonl(INADDR_ANY);
-	/* utilizam un port utilizator */
 	server.sin_port = htons(PORT);
 
-	/* atasam socketul */
 	if (bind(sd, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1)
 	{
-		perror("Eroare la bind().\n");
+		perror("Error on bind().\n");
 		return errno;
 	}
 
-	/* punem serverul sa asculte daca vin clienti sa se conecteze */
 	if (listen(sd, 1) == -1)
 	{
-		perror("Eroare la listen().\n");
+		perror("Error on listen().\n");
 		return errno;
 	}
 
-	/* servim in mod concurent clientii... */
-	while (1)
+	while (1) // Asigura conexiunea la toti clientii
 	{
 		int client;
 		int length = sizeof(from);
@@ -302,13 +267,11 @@ int main()
 		printf("Listening on port %d...\n", PORT);
 		fflush(stdout);
 
-		/* acceptam un client (stare blocanta pina la realizarea conexiunii) */
 		client = accept(sd, (struct sockaddr *)&from, &length);
 
-		/* eroare la acceptarea conexiunii de la un client */
 		if (client < 0)
 		{
-			perror("Eroare la accept().\n");
+			perror("Error on accept().\n");
 			continue;
 		}
 
@@ -320,7 +283,6 @@ int main()
 		}
 		else if (pid > 0)
 		{
-			// parinte
 			close(client);
 			continue;
 		}
@@ -329,21 +291,18 @@ int main()
 
 			while (1)
 			{
-				// copil
 				close(sd);
-				/* s-a realizat conexiunea, se astepta mesajul */
 				bzero(input, 1000);
 				printf("Welcome to TopMusic - Server\n");
 				printf("Awaiting input from client...\n");
 				printf("LOGIN FLAG = %d , ADMIN FLAG = %d \n", loginFlag, adminFlag);
 				fflush(stdout);
 
-				/* citirea mesajului */
 				if (read(client, input, BUF) <= 0)
 				{
-					perror("Eroare la read() de la client.\n");
-					close(client); /* inchidem conexiunea cu clientul */
-					continue;	  /* continuam sa ascultam */
+					perror("Error on read() from client.\n");
+					close(client);
+					continue;
 				}
 
 				printf(" Input received: %s\n", input);
@@ -365,7 +324,7 @@ int main()
 					bzero(username, 100);
 					bzero(password, 100);
 					read(client, username, BUF); // citeste username de la client (1)
-					printf("Username-ul este %s \n", username);
+					printf("Username is: %s \n", username);
 
 					if (isValidUser(username) == 1)
 					{
@@ -376,7 +335,7 @@ int main()
 						printf("USERNAME VALID \n");
 						insertUser(username);
 						read(client, password, BUF); //citeste parola de la client (3)
-						printf("Parola este %s \n", password);
+						printf("Password is: %s \n", password);
 						insertPassword(username, password);
 					}
 					else
@@ -395,9 +354,9 @@ int main()
 					bzero(username, 100);
 					bzero(password, 100);
 					read(client, username, BUF); // citeste username de la client (1)
-					printf("Username-ul este %s \n", username);
+					printf("Username is: %s \n", username);
 					read(client, password, BUF); // citeste parola de la client (2)
-					printf("Parola este %s \n", password);
+					printf("Password is: %s \n", password);
 					if (isAdmin(username, password) == 1)
 					{
 						printf("IS ADMIN\n");
@@ -435,17 +394,17 @@ int main()
 					char song_name[BUF], song_artist[BUF], song_link[BUF], genre1[BUF], genre2[BUF], genre3[BUF];
 
 					read(client, song_name, BUF); // citeste song_name de la client (1);
-					printf("Song name este %s \n", song_name);
+					printf("Song name is: %s \n", song_name);
 					read(client, song_artist, BUF); // citeste song_artist de la client (2);
-					printf("Song artist este %s \n", song_artist);
+					printf("Song artist is: %s \n", song_artist);
 					read(client, song_link, BUF); // citeste link de la client (3);
-					printf("link este %s \n", song_link);
+					printf("link is: %s \n", song_link);
 					read(client, genre1, BUF); // citeste genre1 de la client (4);
-					printf("Song genre1 este %s \n", genre1);
+					printf("Song genre1 is: %s \n", genre1);
 					read(client, genre2, BUF); // citeste genre2 de la client (5);
-					printf("Song genre2 este %s \n", genre2);
+					printf("Song genre2 is: %s \n", genre2);
 					read(client, genre3, BUF); // citeste genre3 de la client (6);
-					printf("Song genre3 este %s \n", genre3);
+					printf("Song genre3 is: %s \n", genre3);
 					insertSong(song_name, song_artist, song_link, genre1, genre2, genre3);
 					printf("Song inserted successfully.\n");
 				}
@@ -456,9 +415,9 @@ int main()
 				{
 					char genre_name[BUF], genre_description[BUF];
 					read(client, genre_name, BUF); // citeste genre name de la client (1);
-					printf("Song name este %s \n", genre_name);
+					printf("Song name is: %s \n", genre_name);
 					read(client, genre_description, BUF); // citeste genre description de la client (2);
-					printf("Song artist este %s \n", genre_description);
+					printf("Song artist is: %s \n", genre_description);
 					if (strcmp(genre_description, "") == 0)
 						strcat(genre_description, "No description");
 					insertGenre(genre_name, genre_description);
@@ -471,7 +430,7 @@ int main()
 				{
 					printf("A intrat pe functie\n");
 					char gen[1000];
-					strcpy(gen, showGenres());
+					showGenres(gen);
 					printf("GEN: %s\n", gen);
 					write(client, gen, 1000); // trimite la client genres
 				}
@@ -485,17 +444,6 @@ int main()
 					strcpy(output, "Error! Unknown command.\n");
 					bzero(input, BUF);
 				}
-
-				/* printf(" Sending response back to client: %s\n", output);
-
-				 //returnam mesajul clientului
-				if (write(client, output, BUF) <= 0)
-				{
-					perror("Eroare la write() catre client.\n");
-					continue; // continuam sa ascultam
-				}
-				else
-					printf(" Response sent successfully.\n"); */
 			} //while
 		}	 // else if
 	}		  //while
